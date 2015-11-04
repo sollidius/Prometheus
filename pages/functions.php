f101712a57fabccad9fbd85d6a21a1cc0a9739ee
Ne00n <neo@tiefkuehler-madness.me>
2015-11-04 11:15:09 +0100

    User Add function

--- pages/functions.php
+++ pages/functions.php
@@ -3,6 +3,54 @@
 $mysqli = new mysqli("localhost", "Prometheus", "aTFGbJjEC9LtUSN4", "prometheus");
 
 
+function user_exists($user,$mysqli) {
+
+  $query = "SELECT `name` FROM `users` WHERE name=?";
+
+  if ($stmt = $mysqli->prepare($query)){
+
+          $stmt->bind_param("s", $user);
+
+          if($stmt->execute()){
+              $stmt->store_result();
+
+              $check= "";
+              $stmt->bind_result($check);
+              $stmt->fetch();
+
+              if ($stmt->num_rows == 1){
+              return true;
+            } else {
+              return false;
+            }
+          }
+      }
+}
+
+function email_exists($email,$mysqli) {
+
+  $query = "SELECT `email` FROM `users` WHERE email=?";
+
+  if ($stmt = $mysqli->prepare($query)){
+
+          $stmt->bind_param("s", $email);
+
+          if($stmt->execute()){
+              $stmt->store_result();
+
+              $check= "";
+              $stmt->bind_result($check);
+              $stmt->fetch();
+
+              if ($stmt->num_rows == 1){
+              return true;
+            } else {
+              return false;
+            }
+          }
+      }
+}
+
 
 
 
--- pages/users.php
+++ pages/users.php
@@ -35,11 +35,140 @@ if ($_SESSION['login'] == 1 and $db_rank == 1) {
            </div>
            <div class="row">
                <div class="col-lg-8">
+                 <?php
+                  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+
+                     if (isset($_POST['confirm'])) {
+
+                       $error = false;
+                       $rank = 2;
+                       $name = $_POST['name']; $email = $_POST['email']; $password = $_POST['pwd1'];
+
+                       if (isset($_POST['administrator'])) { $rank = 1;}
+
+                       if ($_POST['pwd1'] != $_POST['pwd2']) {$error = true;}
+                       if (user_exists($name,$mysqli) == true) { $error = true;}
+                       if (email_exists($email,$mysqli) == true) { $error = true;}
+                       if (strlen($name) <= 3) {$error = true;}
+                       if (strlen($email) <= 3) {$error = true;}
+                       if (strlen($password) <= 7) {$error = true;}
+
+                       if ($error == false) {
+
+                         $hash = password_hash($password, PASSWORD_DEFAULT);
+
+                         $stmt = $mysqli->prepare("INSERT INTO users(name,email,password,rank) VALUES (?, ?, ?, ?)");
+                         $stmt->bind_param('sssi', $name, $email,$hash,$rank);
+                         $stmt->execute();
+                         $stmt->close();
+
+                         echo '
+                         <div class="alert alert-success" role="alert">
+                           <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+                           <span class="sr-only">Error:</span>
+                           Okay
+                         </div>';
+
+                     } else {
+
+                       echo '
+                       <div class="alert alert-danger" role="alert">
+                         <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+                         <span class="sr-only">Error:</span>
+                         Something went wrong
+                       </div>';
+
+                     }
 
 
 
 
-               </div>
+
+
+                    } else {
+
+                  ?>
+
+                  <form class="form-horizontal" action="index.php?page=users" method="post">
+                    <div class="form-group">
+                      <label class="control-label col-sm-2">Name:</label>
+                      <div class="col-sm-8">
+                        <input type="text" class="form-control" name="name" placeholder="Enter Name">
+                      </div>
+                      <div class="checkbox col-sm-2">
+                        <label><input type="checkbox" name ="administrator" value="1">Administrator</label>
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <label class="control-label col-sm-2" for="email">E-Mail:</label>
+                      <div class="col-sm-10">
+                        <input type="email" class="form-control" name="email" placeholder="Enter email">
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <label class="control-label col-sm-2" for="pwd">Passwort:</label>
+                      <div class="col-sm-10">
+                        <input type="password" class="form-control" name="pwd1" placeholder="Enter password">
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <label class="control-label col-sm-2" for="pwd">Passwort Nochmal:</label>
+                      <div class="col-sm-10">
+                        <input type="password" class="form-control" name="pwd2" placeholder="Enter password">
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <div class="col-sm-offset-2 col-sm-10">
+                        <button type="submit" name="confirm" class="btn btn-default">Submit</button>
+                      </div>
+                    </div>
+                  </form>
+
+
+
+                  <?php }
+                  } else {
+                    ?>
+                    <form action="index.php?page=users" method="post">
+                    <button style="margin-bottom:2px;" type="submit" name="add" class="btn pull-right btn-success">+</button>
+                    <table class="table table-bordered">
+                      <thead>
+                        <tr>
+                          <th>Name</th>
+                          <th>E-Mail</th>
+                          <th>Rank</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                     <?php
+
+                     $query = "SELECT name, email, rank FROM users ORDER by id";
+
+                      if ($stmt = $mysqli->prepare($query)) {
+                          $stmt->execute();
+                          $stmt->bind_result($db_name, $db_email,$db_rank);
+
+                          while ($stmt->fetch()) {
+                            echo "<tr>";
+                            echo "<td>" . $db_name . "</td>";
+                            echo "<td>" . $db_email . "</td>";
+                            if ($db_rank == 1) {
+                              echo "<td>Administrator</td>";
+                            } elseif ($db_rank == 2) {
+                              echo "<td>User</td>";
+                            }
+                            echo "</tr>";
+                          }
+                          $stmt->close();
+                      }
+                      $mysqli->close(); ?>
+                      </tbody>
+                    </table>
+                  </form>
+                  <?php }
+                 ?>
+            </div>
                <!-- /.col-lg-8 -->
                <div class="col-lg-4">
 
