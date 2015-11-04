a118e24d3a3162d767c37f68b290d590e8685487
Ne00n <neo@tiefkuehler-madness.me>
2015-11-04 17:15:19 +0100

    SQL

--- prometheus.sql
+++ prometheus.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.12
+-- version 4.5.0.2
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Erstellungszeit: 04. Nov 2015 um 10:04
--- Server-Version: 5.6.25
--- PHP-Version: 5.6.11
+-- Host: localhost
+-- Erstellungszeit: 04. Nov 2015 um 16:14
+-- Server-Version: 5.5.46-0+deb7u1
+-- PHP-Version: 5.6.15-1~dotdeb+zts+7.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,27 +23,59 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Tabellenstruktur für Tabelle `dedicated`
+--
+
+CREATE TABLE `dedicated` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `ip` varchar(15) NOT NULL,
+  `port` int(6) NOT NULL,
+  `user` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `dedicated`
+--
+
+INSERT INTO `dedicated` (`id`, `name`, `ip`, `port`, `user`, `password`, `status`) VALUES
+(1, 'Test', '127.0.0.1', 22, 'root', 'root', 0),
+(2, 'Test', '127.0.0.1', 22, '11', '11', 0),
+(3, 'Test', '127.0.0.1', 22, 'rr', 'rr', 0),
+(4, 'Test', '127.0.0.1', 22, 'prom', '22', 0);
+
+-- --------------------------------------------------------
+
+--
 -- Tabellenstruktur für Tabelle `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `name` varchar(25) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `rank` int(11) NOT NULL DEFAULT '2'
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Daten für Tabelle `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `rank`) VALUES
-(2, 'Test', '123@123.de', '$2y$10$lN.se4AsT6yR0v7FxSoyYuVGi3UPrMjBvgWoNs0ZZc59ghHO7hP2q', 1);
+(17, 'Test', '123@123.de', '$2y$10$J15ufgFaMcksKAMxd73O.e4hRxySkQoKN3hOoe9abM29uRET0Zcqu', 1);
 
 --
 -- Indizes der exportierten Tabellen
 --
+
+--
+-- Indizes für die Tabelle `dedicated`
+--
+ALTER TABLE `dedicated`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indizes für die Tabelle `users`
@@ -56,10 +88,15 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT für Tabelle `dedicated`
+--
+ALTER TABLE `dedicated`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
 -- AUTO_INCREMENT für Tabelle `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
